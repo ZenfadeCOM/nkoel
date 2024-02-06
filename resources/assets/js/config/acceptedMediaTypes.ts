45edef1b68e324a4ed96ba1d8203c2378e4544ab
BuildTools <unconfigured@null.spigotmc.org>
2024-02-05 20:47:19 -0500

    test

---
+++ resources/assets/js/config/acceptedMediaTypes.ts
@@ -0,0 +1,9 @@
+export const acceptedMediaTypes = [
+  'audio/flac',
+  'audio/mp3',
+  'audio/mpeg',
+  'audio/ogg',
+  'audio/x-flac',
+  'audio/x-aac',
+  'audio/wav',
+]
---
+++ resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends('base')
+
+@section('title', 'NANOMUSIC')
+
+@push('scripts')
+    @vite(['resources/assets/js/app.ts'])
+@endpush
